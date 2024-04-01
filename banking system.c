e7c962e7c5844c7ed3bdbdb3f3888dc3a7607cfa
Omaetee <159876554+Omaetee@users.noreply.github.com>
2024-04-01 13:13:52 +0300

    Update banking system.c

--- banking system.c
+++ banking system.c
@@ -5,3 +5,24 @@
 //3.TUM KEITH KIMUTAI ENE212-0092/2021
 //4.TEDDY GITHUGU KAMAU ENE 212-0175/2021
 //5.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//Initializing variables
+    float total_amount, transfer, deposit, withdraw, check_balance;
+    int pin, password, user_input;
+
+//Prompting the user to create an account and enter password
+    printf("Enter password to access account: ");
+    scanf("%d", &password);
+    printf("Enter amount to create account: ");
+    scanf("%f", &total_amount);
+
+//Prompting the user to choose an action
+    printf("Enter 1 for check balance.\nEnter 2 for deposit.\nEnter 3 for withdraw.\nEnter 4 for transfer.\n");
+    scanf("%d", &user_input);
+    printf("Enter pin: ");
+    scanf("%d", &pin);
